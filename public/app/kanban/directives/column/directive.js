69d242d413312c3e6a11145b0f010d50ad08e589
jangot <pulin.pavel@gmail.com>
2014-07-21 00:21:19 +0400

    remove trash

--- public/app/kanban/directives/column/directive.js
+++ public/app/kanban/directives/column/directive.js
@@ -16,23 +16,6 @@ define([
                 columnId: '=column'
             },
             templateUrl: '/app/kanban/directives/column/template.html',
-            link: function(scope, element, attrs) {
-
-                scope.getIndexByOffset = function(offsetY) {
-
-                    var result = 0;
-                    var li = element.find('li')
-                    li.each(function(index) {
-                        console.log(offsetY, this.offsetTop)
-                        if (offsetY < this.offsetTop) {
-                            result = index;
-                            return false;
-                        }
-                    });
-
-                    return 0;
-                }
-            },
             controller: Controller
         }
     });
